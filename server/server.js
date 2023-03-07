53755eb0c6cb798f63969226bb211fd9e024794d
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2023-03-07 20:43:29 +0300

    getting data from the database and sending it to the client.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,50 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require('body-parser'); 
+const mysql = require("mysql2");
+const dbConfig = require("./config/db.config.js");
+
+// const formidable = require('formidable');
+// const fs = require('fs');
+// const path = require('path');
+// const multer = require('multer');
+
+const connection = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    database: dbConfig.database,
+    password: dbConfig.password
+});
+
+connection.connect(function(err){
+    if (err) {
+        return console.error("Ошибка: " + err.message);
+    }
+    else {
+        console.log("Подключение к серверу MySQL успешно установлено");    
+    }
+}); 
 
 const app = express();
+const port = 5000;
+// const urlencodedParser = express.urlencoded({extended: false});
+// const sorting_info = ["spending_id", true]; 
 
-// app.use(express.static(path.join(__dirname, 'build')));
- app.use(bodyParser.json());
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+// app.use('/css', express.static(__dirname + 'public/css'));
+// app.use('/js', express.static(__dirname + 'public/js'));
+// app.use('/img', express.static(__dirname + 'public/img'));
+// app.use('/uploads', express.static(__dirname + 'public/uploads'));
+
 
 app.get('/api', (req, res) => {
-    res.json({"sums": ['10', '12', '158', '15', 100, 0, 200000]});
-   });
+    connection.query('SELECT * FROM Spending', function (err, result) {
+        if (err) throw err;
+        res.json({"items": result});
+    });
+    
+});
 
 // app.get('/api/getRequest', (req, res) => {
 //  //API logic
@@ -23,6 +58,13 @@ app.get('/api', (req, res) => {
 //  res.sendFile(path.join(__dirname, 'build/index.html'));
 // });
 
-app.listen(5000, () => {
- console.log('Listening on port ', 5000);
+app.listen(port, () => {
+ console.log('Listening on port ', port);
 });
+
+function get_all(response) {
+    connection.query('SELECT * FROM Spending', function (err, result) {
+        if (err) throw err;
+        response.render('index', { fins: result, title: 'Трекер финансов', sorting_info:sorting_info, submitValue: "Добавить" });
+    });
+}
