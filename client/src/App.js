3d7ae81d3b32e122cc8470a0c52d40bf527e4af5
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2023-03-09 02:21:51 +0300

    added items editing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Moment from 'moment';
-import { FiTrash, FiEdit, FiChevronUp, FiChevronDown } from 'react-icons/fi';
+import { FiTrash, FiEdit, FiChevronUp, FiChevronDown, FiX } from 'react-icons/fi';
 import './App.css';
 
 class InputForm extends React.Component {
@@ -9,14 +9,41 @@ class InputForm extends React.Component {
     this.onChange = this.handleChange.bind(this);
     this.onSubmit = this.handleSubmit.bind(this);
     this.onPickedFile = this.handlePickedFile.bind(this);
+
     this.state = {
-      body: {sum: "", category: "", description: "", date: "", type: "expenses", filename: null}, 
-      selectedFile: "", 
-      inputKey: 0};
+      body: {sum: "", category: "", description: "", date: this.setDefaultDate(""), type: "expenses", filename: null}, 
+      selectedFile: null, 
+      inputKey: 0,
+      isAdd: true};
   }
 
+  setDefaultDate(strdate){ 
+    let date ; 
+    if(strdate === "") {
+      date = new Date();
+    } else {
+      date = new Date(strdate);
+    }
+    date.setMilliseconds(3 * 60 * 60 * 1000);
+    return date.toISOString().substring(0, 10);
+  }
 
-	handlePickedFile(e){
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.updItem !== null)
+    {
+      this.setState({body: {
+        sum: nextProps.updItem.sum / 100, 
+        category: nextProps.updItem.category, 
+        description: nextProps.updItem.description, 
+        date: this.setDefaultDate(nextProps.updItem.date), 
+        type: nextProps.updItem.income === 1 ? 'income' : "expenses", 
+        spending_id: nextProps.updItem.spending_id,
+        filename: null},
+      isAdd: false});
+    }
+  }
+
+	handlePickedFile(e) {
 		this.setState({selectedFile: e.target.files[0]});
     console.log(e.target.files[0]);
 	};
@@ -29,32 +56,39 @@ class InputForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.addPosts(this.state.body, this.state.selectedFile);
+    if (this.state.isAdd) {
+      this.props.addItems(this.state.body, this.state.selectedFile);
+    } else {
+        this.props.updateItems(this.state.body, this.state.selectedFile);
+    }
+
     this.setState({
-      body: {sum: "", category: "", description: "", date: "", type: "expenses", filename: null}, 
+      body: {sum: "", category: "", description: "", date: this.setDefaultDate(""), type: "expenses", filename: null}, 
       selectedFile: null, 
-      inputKey: Date.now()
+      inputKey: Date.now(),
+      isAdd: true
     });  
   };  
-  
+
   render() {
     return (
-      <form className="decor" name="inputData" id="iForm" 
-        onSubmit={this.onSubmit}
-      >
+      <form className="decor" name="inputData" onSubmit={this.onSubmit}>
         <div className="form-inner">
             <h3>Введите данные</h3>
             <input type="number" step="0.01" min="0" max="42949672.90" name="sum" placeholder="Сумма" required 
-            onChange={this.onChange} value={this.state.body.sum}
-            />            
-            <input type="text" name="category" maxLength="50" placeholder="Категория" onChange={this.onChange} value={this.state.body.category}/>         
-            <input type="date" id="datePicker" name="date" placeholder="Дата" max="9999-12-31" onChange={this.onChange} value={this.state.body.date}/>   
-            <textarea placeholder="Описание..." rows="6" name="description" onChange={this.onChange} value={this.state.body.description}></textarea>  
-            <input type="radio" id="radio-1" name="type" value="expenses" checked={this.state.body.type === "expenses"} onChange={this.onChange}/>      
+              onChange={this.onChange} value={this.state.body.sum}/>            
+            <input type="text" name="category" maxLength="50" placeholder="Категория"  required 
+              onChange={this.onChange} value={this.state.body.category}/>         
+            <input type="date" id="datePicker" name="date" placeholder="Дата" max="9999-12-31" required 
+              onChange={this.onChange} value={this.state.body.date}/>   
+            <textarea placeholder="Описание..." rows="6" name="description" required 
+              onChange={this.onChange} value={this.state.body.description}></textarea>  
+            <input type="radio" id="radio-1" name="type" value="expenses" 
+              checked={this.state.body.type === "expenses"} onChange={this.onChange}/>      
             <label htmlFor="radio-1">Расходы</label>  
-            <input type="radio" id="radio-2" name="type" value="income" checked={this.state.body.type === "income"} onChange={this.onChange}/>      
+            <input type="radio" id="radio-2" name="type" value="income" 
+              hecked={this.state.body.type === "income"} onChange={this.onChange}/>      
             <label htmlFor="radio-2">Доходы</label>
-
             <input type="file" name="fileToUpload" key={this.state.inputKey} onChange={this.onPickedFile}/>
             <input type="submit" value="Добавить" /> 
         </div>
@@ -147,9 +181,11 @@ class DataTable extends React.Component {
             <td>{item.description}</td>
             <td>{Moment(item.date).format('DD.MM.YYYY')}</td>
             <td>{item.income ? "Доходы" : "Расходы"}</td>
-            <td><a href={"http://localhost:5000/api/" + item.filename} target="_blank" >{item.filename}</a></td>
+            <td><a href={"http://localhost:5000/api/" + item.filename} target="_blank" >{item.filename}</a>
+              {item.filename != null ? <FiX onClick={() => this.props.deleteFile(item.spending_id)} /> : null }              
+            </td>
             <td><FiTrash onClick={() => this.props.onClickDelete(item.spending_id)} /></td>
-            <td><FiEdit /></td> 
+            <td><FiEdit onClick={() => this.props.onClickUpdate(item)}/></td> 
             </tr>  
           ))} 
         </tbody>  
@@ -160,67 +196,130 @@ class DataTable extends React.Component {
 
 function App() {
   const [items, setItems] = useState([]);
+  const [updItem, setUpdItem] = useState(null);
   
-    useEffect(() => {
-      fetch("/api", {method: "GET"})
-        .then(response => response.json())
-        .then(data => { setItems(data.items) })
-    }, [])
+  useEffect(() => {
+    fetch("/api", {method: "GET"})
+      .then(response => response.json())
+      .then(data => { setItems(data.items) })
+  }, [])
 
-    const deleteItem = async (id) => {
-       await fetch('/api/' + id,{ method: 'DELETE',})
-        .then((response) => {
-            if (response.status === 200) {
-             setItems(actualItems => actualItems.filter(data => data.spending_id !== id));
-            }
-        }); 
-      };
+  const deleteItem = async (id) => {
+    await fetch('/api/' + id,{ method: 'DELETE',})
+    .then((response) => {
+        if (response.status === 200) {
+          setItems(actualItems => actualItems.filter(data => data.spending_id !== id));
+        }
+    }); 
+  };
 
-      const addPosts = async (body, selectedFile) => { 
-        const formData = new FormData();        
-        formData.append('sum', body.sum);
-        formData.append('category', body.category);
-        formData.append('description', body.description);
-        formData.append('date', body.date);
-        formData.append('type', body.type);
-        formData.append('fileToUpload', selectedFile);
+  const deleteFile = async (id) => { 
+    const formData = new FormData();        
+    formData.append('spending_id', id);
 
-        await fetch('/api', {
-           method: 'POST',
-           body: formData
-        })
-           .then((response) => response.json())
-           .then((data) => {
-            // if (response.status === 200) {
-              body["spending_id"] = data.spending_id;
-              body["sum"] *= 100;
-              body["filename"] = selectedFile.name;
-              setItems((items) => [...items, body]);
-            // }
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
-     };
+    await fetch('/api/deleteFile', {
+      method: 'PUT',
+      body: formData
+    })
+    .then((response) => {
+      response.json();
+      if (response.status === 200) {              
+        setItems(prevItems => prevItems.map(item => item.spending_id === id ? { ...item, filename: null } : item));
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
 
-    return (
-      <div className="wrapper">
-        <div className="content">
-          <InputForm 
-            addPosts={addPosts}
-          />
-          {(items.length !== 0) ? (
-          <DataTable 
-            items={items}
-            onClickDelete={deleteItem}
-          />          
-        ) : (
-          <p>Loading...</p> 
-        )}  
-        </div> 
-        <footer>Copyright &copy; 2023 | Made by Abakanovich</footer>  
-      </div>
-    )
-  }
+  const fillForm = async (item) => {
+    setUpdItem(item);
+    console.log(item);
+  };
 
-  export default App; 
+  const addItems = async (body, selectedFile) => { 
+    const formData = new FormData();        
+    formData.append('sum', body.sum);
+    formData.append('category', body.category);
+    formData.append('description', body.description);
+    formData.append('date', body.date);
+    formData.append('type', body.type);
+    formData.append('fileToUpload', selectedFile);
+
+    await fetch('/api', {
+        method: 'POST',
+        body: formData
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      body["spending_id"] = data.spending_id;
+      body["sum"] *= 100;
+      if (selectedFile !== null)
+        body["filename"] = selectedFile.name;
+      else
+        body["filename"] = null;
+      setItems((items) => [...items, body]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
+
+  const updateItems = async (body, selectedFile) => { 
+    const formData = new FormData();        
+    formData.append('sum', body.sum);
+    formData.append('category', body.category);
+    formData.append('description', body.description);
+    formData.append('date', body.date);
+    formData.append('type', body.type);
+    formData.append('spending_id', body.spending_id);
+    formData.append('fileToUpload', selectedFile);
+
+    await fetch('/api', {
+        method: 'PUT',
+        body: formData
+    })
+    .then((response) => {
+      response.json();
+      if (response.status === 200) {          
+        if (selectedFile !== null)
+          setItems(prevItems => prevItems.map(item => item.spending_id === body.spending_id ? 
+            { ...item, sum: body.sum * 100, user_id: 1, category: body.category, 
+              description: body.description, income: body.type === "income" ? 1 : 0, date: new Date(body.date), 
+              filename: selectedFile.name} : item));
+        else
+        setItems(prevItems => prevItems.map(item => item.spending_id === body.spending_id ? 
+          { ...item, sum: body.sum * 100, user_id: 1, category: body.category, 
+            description: body.description, income: body.type === "income" ? 1 : 0, date: new Date(body.date)} : item));
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <InputForm 
+          addItems={addItems}
+          updateItems={updateItems}
+          updItem={updItem}
+        />
+        {(items.length !== 0) ? (
+        <DataTable 
+          items={items}
+          onClickDelete={deleteItem}
+          onClickUpdate={fillForm}
+          deleteFile={deleteFile}
+        />          
+      ) : (
+        <p>Loading...</p> 
+      )}  
+      </div> 
+      <footer>Copyright &copy; 2023 | Made by Abakanovich</footer>  
+    </div>
+  )
+}
+
+export default App; 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
 const express = require('express');
+const multer = require('multer');
 const bodyParser = require('body-parser'); 
 const mysql = require("mysql2");
-const multer = require('multer');
 const dbConfig = require("./config/db.config.js");
-
-// const formidable = require('formidable');
-// const fs = require('fs');
-// const path = require('path');
-// const multer = require('multer');
 
 const connection = mysql.createConnection({
     host: dbConfig.host,
@@ -37,60 +32,40 @@ const storage = multer.diskStorage ({
         cb(null, file.originalname);
     }
 })
-const upload = multer({storage: storage})
-
+const upload = multer({storage: storage});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-// app.use('/css', express.static(__dirname + 'public/css'));
-// app.use('/js', express.static(__dirname + 'public/js'));
-// app.use('/img', express.static(__dirname + 'public/img'));
-// app.use('/uploads', express.static(__dirname + 'public/uploads'));
-
-
-// app.get('/api', (req, res) => {
-//     connection.query('SELECT * FROM Spending', function (err, result) {
-//         if (err) throw err;
-//         res.json({"items": result});
-//     });
-    
-// });
 
 app.get('/api/:filename', (req, res) => {
-
     res.status(200).sendFile(__dirname + '/public/uploads/' + req.params.filename, (err, file) => {
-        if (err) res.sendStatus(400);
+        if (err) res.sendStatus(404);
         res.end(file);
       });
 });
 
 app.get("/api", function(req, res){      
     connection.query('SELECT * FROM Spending', function (err, result) {
-        if (err) throw err;
+        if (err) res.sendStatus(400);
         return res.status(200).json({"items": result});
     });
 });
 
-// удаление пользователя по id
-app.delete("/api/:id", function(req, res){
-       
+app.delete("/api/:id", function(req, res){       
     const id = req.params.id;
     connection.query('DELETE FROM Spending WHERE spending_id=' + id 
                         + ' AND user_id=1', function (err, result) {
-        if (err) throw err;
+        if (err) res.sendStatus(400);
         if (result.affectedRows === 0)
-            res.status(404).send({ message: 'Unable delete item.' })
+            res.status(404).send({ message: 'Unable delete item' })
         else
-            res.status(200).send({ message: 'Item deleted.' })        
+            res.status(200).send({ message: 'Deleted item with id=' + id })        
     });
 });
 
 app.post("/api", upload.single('fileToUpload'), urlencodedParser, function (req, res) {
       
-    console.log(req.body);
-    console.log(req);
-    if(!req.body) return res.sendStatus(400);
-      
+    if(!req.body) return res.sendStatus(400);     
 
     connection.query('INSERT Spending(user_id, sum, date, category, description, income, filename) VALUES (?,?,?,?,?,?,?)',
     [
@@ -99,7 +74,7 @@ app.post("/api", upload.single('fileToUpload'), urlencodedParser, function (req,
     req.body.date,
     req.body.category,
     req.body.description,
-    req.body.type == 'income',
+    req.body.type === 'income',
     req.file ? req.file.originalname : null
     ], function (err, result) {
         if (err) throw err;
@@ -107,124 +82,56 @@ app.post("/api", upload.single('fileToUpload'), urlencodedParser, function (req,
     }); 
 });
 
-// app
-//   .route('/api')
-//   .get((req, res) => {
-//     // if (req.query.id) {
-//     //   if (req.users.hasOwnProperty(req.query.id))
-//     //     return res
-//     //       .status(200)
-//     //       .send({ data: req.users[req.query.id] })
-//     //   else
-//     //     return res
-//     //       .status(404)
-//     //       .send({ message: 'User not found.' })
-//     // } else if (!req.users)
-//     //   return res
-//     //     .status(404)
-//     //     .send({ message: 'Users not found.' })
+app.put("/api", upload.single('fileToUpload'), urlencodedParser, function(req, res){
+  
+    if(!req.body) return res.sendStatus(400);
+    
+    console.log(req.body)
+    if (req.file !== null) {
+        connection.query('UPDATE Spending SET sum = ?, date = ?, category = ?, description = ?, income = ?, filename = ? WHERE spending_id = ?  AND user_id=1',
+            [
+                req.body.sum * 100,
+                req.body.date,
+                req.body.category,
+                req.body.description,
+                req.body.type === 'income',
+                req.file ? req.file.originalname : null,
+                req.body.spending_id
+            ], function (err, result) {
+                if (err) throw err;
+                res.status(200).send({ message: 'Update item with id=' + req.body.spending_id });
+            }); 
+    } else {
+        connection.query('UPDATE Spending SET sum = ?, date = ?, category = ?, description = ?, income = ? WHERE spending_id = ?  AND user_id=1',
+        [
+            req.body.sum * 100,
+            req.body.date,
+            req.body.category,
+            req.body.description,
+            req.body.type === 'income',
+            req.body.spending_id
+        ], function (err, result) {
+            if (err) throw err;
+            res.status(200).send({ message: 'Update item with id=' + req.body.spending_id});
+        }); 
+    }
+});
 
-//     connection.query('SELECT * FROM Spending', function (err, result) {
-//         if (err) throw err;
-//         return res.status(200).json({"items": result});
-//     });
+app.put("/api/deleteFile", upload.single('fileToUpload'), urlencodedParser, function(req, res){
+  
+    if(!req.body) return res.sendStatus(400);
+    console.log(req.body)
 
-//     // return res.status(200).send({ data: req.users })
-//   })
-//   .post((req, res) => {
-//     if (req.body.user && req.body.user.id) {
-//       if (req.users.hasOwnProperty(req.body.user.id))
-//         return res
-//           .status(409)
-//           .send({ message: 'User already exists.' })
+    connection.query('UPDATE Spending SET filename = null WHERE spending_id = ?  AND user_id=1',
+        [
+            req.body.spending_id
+        ], function (err, result) {
+            if (err) throw err;
+            res.status(200).send({ message: 'File delete from item with id=' + req.body.spending_id});
+        }); 
 
-//       req.users[req.body.user.id] = req.body.user
-
-//       fs.writeFile(
-//         file,
-//         JSON.stringify(req.users),
-//         (err, response) => {
-//           if (err)
-//             return res
-//               .status(500)
-//               .send({ message: 'Unable create user.' })
-
-//           return res
-//             .status(200)
-//             .send({ message: 'User created.' })
-//         }
-//       )
-//     } else
-//       return res
-//         .status(400)
-//         .send({ message: 'Bad request.' })
-//   })
-//   .put((req, res) => {
-//     if (req.body.user && req.body.user.id) {
-//       if (!req.users.hasOwnProperty(req.body.user.id))
-//         return res
-//           .status(404)
-//           .send({ message: 'User not found.' })
-
-//       req.users[req.body.user.id] = req.body.user
-
-//       fs.writeFile(
-//         file,
-//         JSON.stringify(req.users),
-//         (err, response) => {
-//           if (err)
-//             return res
-//               .status(500)
-//               .send({ message: 'Unable update user.' })
-
-//           return res
-//             .status(200)
-//             .send({ message: 'User updated.' })
-//         }
-//       )
-//     } else
-//       return res
-//         .status(400)
-//         .send({ message: 'Bad request.' })
-//   })
-//   .delete((req, res) => {
-//     console.log(req);
-//     // if (req.query.id) {
-//     //   if (req.users.hasOwnProperty(req.query.id)) {
-//     //     delete req.users[req.query.id]
-
-//     //     fs.writeFile(
-//     //       file,
-//     //       JSON.stringify(req.users),
-//     //       (err, response) => {
-//     //         if (err)
-//     //           return res
-//     //             .status(500)
-//     //             .send({ message: 'Unable delete user.' })
-
-//     //         return res
-//     //           .status(200)
-//     //           .send({ message: 'User deleted.' })
-//     //       }
-//     //     )
-//     //   } else
-//     //     return res
-//     //       .status(404)
-//     //       .send({ message: 'User not found.' })
-//     // } else
-//       return res
-//         .status(400)
-//         .send({ message: 'Bad request.' })
-//   })
-
+});
 
 app.listen(port, () => {
  console.log('Listening on port ', port);
 });
-
-function get_all(response) {
-    connection.query('SELECT * FROM Spending', function (err, result) {
-        if (err) throw err;
-        response.render('index', { fins: result, title: 'Трекер финансов', sorting_info:sorting_info, submitValue: "Добавить" });
-    });
-}
